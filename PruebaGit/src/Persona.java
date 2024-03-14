7a2e3b9673a31ae3bcb655d520f4ca6ad236d205
unknown <alb0026@alu.medac.es>
2024-03-14 16:07:24 +0100

    proyecto personas creado

---
+++ PruebaGit/src/Persona.java
@@ -0,0 +1,112 @@
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    private String dni;
+
+    /**
+     * 
+     */
+    private String nombre;
+
+    /**
+     * 
+     */
+    private String apellido;
+
+    /**
+     * 
+     */
+    private int edad;
+
+    /**
+     * @param dni 
+     * @param nombre 
+     * @param apellido 
+     * @param edad
+     */
+    public void Persona(String dni, String nombre, String apellido, int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * Modifica el DNI de la persona
+     * @param dni
+     */
+    public void setDni(String dni) {
+        // TODO implement here
+    }
+
+    /**
+     * Devuelve el DNI de la persona
+     * @return
+     */
+    public String getDni() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * Modifica el nombre de la persona
+     * @param nombre
+     */
+    public void setNombre(String nombre) {
+        // TODO implement here
+    }
+
+    /**
+     * Devuelve el nombre de la persona
+     * @return
+     */
+    public String getNombre() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * Modifica el apellido de la persona
+     * @param apellido
+     */
+    public void setApellido(String apellido) {
+        // TODO implement here
+    }
+
+    /**
+     * Devuelve el apellido de la persona
+     * @return
+     */
+    public String getApellido() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * Modifica la edad de la persona
+     * @param edad
+     */
+    public void setEdad(int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * Devuelve la edad de la persona
+     * @return
+     */
+    public int getEdad() {
+        // TODO implement here
+        return 0;
+    }
+
+}
